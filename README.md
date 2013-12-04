1a584ff51c9c9775310cdff7fabb72e66d8bca47
Glynn Bird <glynn@scoot.co.uk>
2013-12-04 12:58:59 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# SECRET SANTA
+
+IF you are running a Secret Santa draw in your workplace and need a means of pairing the gifts between sender and receiver, then this is the tool for you. 
+It is a command-line, Node.js script which is run like this:
+
+```
+node secretsanta.js Rita Sue Bob "Alan Sugar"
+```
+
+i.e. you supply a list people on the command-line and the script will produce a list of whose present is destinted for whom, without paper and guaranteeing
+then a sender can't receive his/her own present.
+
+## Installation
+
+Download the repository, run "npm install" and the run the script.
+
+Merry Christmas.
---
+++ secretsanta.js
@@ -0,0 +1,57 @@
+var _ = require('underscore'),
+  people = require('optimist').argv._,              // ["Glynn","Matt","Dan O","Dan M","Rich","Simon","Paul","Helen","Dave"];
+  done = false;
+
+
+if (people.length < 2) {
+ console.log("You must have two or more people for a scret santa");
+ process.exit();
+}
+
+
+// pick until we have a satisfactory outcome
+do {
+  
+  // the final array of picks
+  var picks = {};
+  
+  // the list of present receivers left
+  var receivers = _.clone(people);
+  
+  // loop through each person
+  for (var i in people) {
+  
+    // get the sender's name
+    var s = people[i];
+    var r = null;
+  
+    // find a receiver
+    do {
+    
+      // check for infinite loop - i.e. only one person left to pick who is both sender & receiver
+      if ( receivers.length == 1 && receivers[0] == s) {
+        break;
+      }
+      
+      // pick a receiver at random from the list
+      var j = _.random(0, receivers.length - 1);
+    
+      // a sender can't give a present to themselves
+      if (j != i) {
+        r = receivers[j];
+        receivers.splice(j, 1);
+      }    
+    } while(r == null);
+  
+    // we have a match
+    if ( r != null) {
+      picks[s] = r;
+    }
+  }
+
+} while (_.keys(picks).length < people.length); // until we have a full list of picks
+
+// output the answers
+for(var sender in picks) {
+  console.log(sender," ------> ",picks[sender]);
+}
